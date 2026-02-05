f6bf8e56d618010d3e70b2417d13c97e9d3b26fe
n2004ina <ninaabudjen@gmail.com>
2026-02-04 21:39:18 -0800

    Napravljen proces za servera

---
+++ Server/Proces.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Server
+{
+    [Serializable]
+    public class Proces
+    {
+        public string Naziv { get; set; }
+        public int VrijemeIzvrsavanja { get; set; }
+        public int Prioritet { get; set; }
+        public double ZauzeceProcessora { get; set; }
+        public double ZauzeceMemorije { get; set; }
+
+        public Proces() { }
+
+        public Proces(string naziv, int vrijemeIzvrsavanja, int prioritet,
+                     double zauzeceProcessora, double zauzeceMemorije)
+        {
+            Naziv = naziv;
+            VrijemeIzvrsavanja = vrijemeIzvrsavanja;
+            Prioritet = prioritet;
+            ZauzeceProcessora = zauzeceProcessora;
+            ZauzeceMemorije = zauzeceMemorije;
+        }
+
+        public override string ToString()
+        {
+            return $"Naziv: {Naziv,-15} | Vrijeme: {VrijemeIzvrsavanja,3}s | " +
+                   $"Prioritet: {Prioritet} | CPU: {ZauzeceProcessora,5}% | " +
+                   $"Memorija: {ZauzeceMemorije,5}%";
+        }
+    }
+}
